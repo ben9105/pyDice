e18abf1600fd414b019eb66df69a620f6e4b4cb0
ben9105 <benjaminshew@gmail.com>
2017-01-31 21:34:22 -0800

    added notes

--- diceGame.py
+++ diceGame.py
@@ -4,14 +4,17 @@ import random
 min = 1
 max = 6
 
-
+#player one's turn
 dicePlayer1 = raw_input("Player one, do you want to roll?" )
+#asks first player if they want to go
 if dicePlayer1 == "yes":
     dice1 = random.randrange(min, max)
+    #prints number between 1 and 6
     print "Your dice was", dice1
 else:
     print "Player two is going to win if you say no."
 
+#player two's turn
 dicePlayer2 =raw_input("Player two, do you want to roll? ")
 if dicePlayer2 == "yes":
     dice2 = random.randrange(min, max)
@@ -19,6 +22,7 @@ if dicePlayer2 == "yes":
 else:
     print "That means player one wins"
 
+#determins who won...or tied
 if dice1 > dice2:
     print "Player one wins!"
 elif dice2 > dice1:
