945e881ec719f3dfae3fb99105bc16c95e766b20
ben9105 <benjaminshew@gmail.com>
2017-01-31 21:08:07 -0800

    made game two player

--- diceGame.py
+++ diceGame.py
@@ -4,10 +4,24 @@ import random
 min = 1
 max = 6
 
-dice = raw_input("Are you ready to roll the dice? ")
-if dice == "yes":
-    print random.randrange(min, max)
-elif dice == "y":
-    print random.randrange(min, max)
+
+dicePlayer1 = raw_input("Player one, do you want to roll?" )
+if dicePlayer1 == "yes":
+    dice1 = random.randrange(min, max)
+    print "Your dice was", dice1
 else:
-    print "Are you sure you don't want to roll?"
+    print "Player two is going to win if you say no."
+
+dicePlayer2 =raw_input("Player two, do you want to roll? ")
+if dicePlayer2 == "yes":
+    dice2 = random.randrange(min, max)
+    print "Your dice was", dice2
+else:
+    print "That means player one wins"
+
+if dice1 > dice2:
+    print "Player one wins!"
+elif dice2 > dice1:
+    print "Player two wins!"
+elif dice1 == dice2:
+    print "Tie!"
