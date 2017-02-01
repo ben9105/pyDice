9409f90d669003c16a0719c2e2bb8ca9001f20fd
ben9105 <benjaminshew@gmail.com>
2017-01-31 20:26:20 -0800

    Python Dice Game

---
+++ diceGame.py
@@ -0,0 +1,11 @@
+import math
+import random
+
+min = 1
+max = 6
+
+dice = raw_input("Are you ready to roll the dice? ")
+if dice == "yes":
+    print random.randrange(min, max)
+else:
+    print "Are you sure you don't want to roll?"
